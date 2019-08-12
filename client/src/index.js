e5805af51abfff4966922246cbb79c59e03c8dde
Heather-Ridgill <Heather.Ridgill@gmail.com>
2019-08-11 18:30:14 -0700

    Finished-MVP

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
+// import { Route } from "react-router-dom";
+// import SavedList from "./Movies/SavedList";
+// import MovieList from "./Movies/MovieList";
+// import Movie from "./Movies/Movie";
 
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <Router>
